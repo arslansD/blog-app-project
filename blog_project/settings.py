68ecce1914a5f80e2c0b37383569361302740787
arslansd <arslansultanbekuulu@gmail.com>
2022-09-24 21:30:05 +0600

    added registration and login

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    "accounts.apps.AccountsConfig", # new
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home" # new
